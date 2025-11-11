99d4e7bb2749c09125447096ac9018d3367fb74a
scross52 <scross52@ivytech.edu>
2025-11-10 23:19:45 -0500

    Add flexbox layout

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import styled from "styled-components/native";
+import { Text, View } from 'react-native';
+import styles from "./Styles";
+import Row from "./Row";
+import Column from "./Column";
+import Box from "./Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi, my name is Sawyer, and I am a mobile developer</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={false} />
+      <Row>
+        <Column>
+          <Box>#1</Box>
+          <Box>#2</Box>
+        </Column>
+        <Column>
+          <Box>#3</Box>
+          <Box>#4</Box>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Box>#5</Box>
+          <Box>#6</Box>
+        </Column>
+        <Column>
+          <Box>#7</Box>
+          <Box>#8</Box>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Box>#9</Box>
+          <Box>#10</Box>
+        </Column>
+        <Column>
+          <Box>#11</Box>
+          <Box>#12</Box>
+        </Column>
+      </Row>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: "20px",
-  },
-});
---
+++ Styles.js
@@ -0,0 +1,25 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+  box: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
+  },
+});
