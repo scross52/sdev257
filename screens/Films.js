cb51f80f267440ccbb2674f3daefda13252cbce6
scross52 <scross52@ivytech.edu>
2025-11-24 21:52:02 -0500

    Update all pages with SearchInput

--- screens/Films.js
+++ screens/Films.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View, } from 'react-native';
 import FilmList from '../components/FilmsList';
+import SearchInput from '../components/SearchInput';
 import styles from '../Styles';
 
 export default function Films() {
   return (
     <View style={styles.container}>
+       <SearchInput placeholder={'Search across site...'} />
       <Text style={styles.text}>Films Screen</Text>
       <FilmList />
     </View>
--- screens/Ships.js
+++ screens/Ships.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View, } from 'react-native';
 import ShipsList from '../components/SpaceshipsList';
+import SearchInput from '../components/SearchInput';
 import styles from '../Styles';
 
 export default function Spaceships() {
   return (
     <View style={styles.container}>
+       <SearchInput placeholder={'Search across site...'} />
       <Text style={styles.text}>Spaceships Screen</Text>
       <ShipsList />
     </View>
