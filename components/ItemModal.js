2ee74480c7767f9e031bb4443e1577db8e1b4307
scross52 <scross52@ivytech.edu>
2025-12-02 19:18:53 -0500

    update animation for Modals

--- components/ItemModal.js
+++ components/ItemModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Modal, Text, TouchableOpacity } from "react-native";
+import Animated, { FadeIn, FadeOut, SlideInUp, SlideOutDown} from "react-native-reanimated";
 import PropTypes from "prop-types";
 import styles from "../Styles";
 
@@ -10,8 +11,17 @@ export default function ItemModal({ text, visible, onClose }) {
       animationType="fade"
       transparent={true}
     >
-      <View style={styles.modalBackground}>
-        <View style={styles.modalContent}>
+      <Animated.View 
+        style={styles.modalBackground}
+        entering={FadeIn}
+        exiting={FadeOut}
+      >
+        <Animated.View 
+          style={styles.modalContent}
+          entering={SlideInUp.duration(250)}
+          exiting={SlideOutDown.duration(250)}
+        >
+
           <Text style={styles.modalText}>This Item is:</Text>
           <Text style={styles.modalText}>{text}</Text>
           <TouchableOpacity
@@ -20,8 +30,10 @@ export default function ItemModal({ text, visible, onClose }) {
           >
             <Text style={styles.closeText}>Close</Text>
           </TouchableOpacity>
-        </View>
-      </View>
+
+        </Animated.View>
+
+      </Animated.View>
     </Modal>
   )
 }
